b0d7e8eac6abf92e9f87799cc34612ea24985025
Khalid J. <khalid.y96@outlook.com>
2025-11-11 19:08:57 +0300

    fix version check

--- main.cpp
+++ main.cpp
@@ -76,8 +76,9 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     PHANDLE = handle;
 
     const std::string HASH = __hyprland_api_get_hash();
+    const std::string CLIENT_HASH = __hyprland_api_get_client_hash();
 
-    if (HASH != GIT_COMMIT_HASH) {
+    if (HASH != CLIENT_HASH) {
         HyprlandAPI::addNotification(PHANDLE,
                                      "[minimize-dispatcher] Failure in initialization: Version mismatch "
                                      "(headers ver is not equal to running hyprland ver)",
